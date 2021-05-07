fd59d397160f26cdf9487b86632385a0d3986517
Oscar Otero <oom@oscarotero.com>
2021-05-07 17:56:00 +0200

    updated std deps

--- deno/test/deps.js
+++ deno/test/deps.js
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.77.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.95.0/testing/asserts.ts";
 export { default as postcss } from "https://deno.land/x/postcss/mod.js";
